20d3a80844d8e71acde184b1495423e6d9a2135c
Rosariy <132590902+Rosariy001@users.noreply.github.com>
2023-05-18 16:54:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from pyrogram import Client, filters
 
+
+API_ID = "10349117"
+API_HASH = "fb88362d7567e017155bcc39d4efa059"
+BOT_TOKEN = "5979937723:AAGwJdJktjrxwvcINvMSbaXEmJ1oIOW7Z6k"
+
+
+KDBOTZZ = Client(
+    name="PyrogramBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+print("Bot Started")
+
+KDBOTZZ.run()
